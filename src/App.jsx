b015294077458325d34c85bf8c0ba459c57d956b
dainlee <dlekdls5577@gmail.com>
2025-01-09 16:03:10 +0900

    [이다인] Feat: todo 리스트 추가 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,46 @@
 import "./App.css";
-import { useState } from "react";
-
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 import { getDate } from "./utils/getDate";
+import { useState, useRef } from "react";
 
 const mockData = [
-{
-	id: 0,
-	isDone: false,
-	content: "React 공부하기",
-	data: getDate(new Date()),
-},
-{
-	id: 1,
-	isDone: false,
-	content: "빨래하기",
-	data: getDate(new Date()),
-},
-{
-	id: 2,
-	isDone: false,
-	content: "노래 연습하기",
-	data: getDate(new Date()),
-},
-]
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: getDate(new Date()),
+  },
+  { id: 1, isDone: false, content: "빨래하기", date: getDate(new Date()) },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: getDate(new Date()),
+  },
+];
 
 function App() {
   const [todos, setTodos] = useState(mockData);
-  console.log(todos)
+  const idRef = useRef(3);
 
+  console.log(todos);
+
+  const onCreate = (content) => {
+    const newData = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: getDate(new Date()),
+    };
+    setTodos([newData, ...todos]); // 스프레드 연산자
+  };
   return (
-    <div className="flex flex-col gap-8 w-96 mx-auto my-0 font-sans">
+    <div className="flex flex-col gap-10 w-96 mx-auto my-0 font-sans">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
     </div>
   );
 }
