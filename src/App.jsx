9895c7f0fe34939bde9c5a878aa95ab448fc6e7b
unknown <somi5488@naver.com>
2025-01-10 16:38:09 +0900

    [김은솜] Feat: 전체적인 UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import { getDate } from "./utils/getDate";
+import { useState, useRef } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: getDate(new Date()),
+  },
+  { id: 1, isDone: false, content: "빨래하기", date: getDate(new Date()) },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: getDate(new Date()),
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
 
+  console.log(todos);
+
+  const onCreate = (content) => {
+    const newData = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: getDate(new Date()),
+    };
+    setTodos([newData, ...todos]); // 스프레드 연산자
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col gap-10 w-96 mx-auto my-0 font-sans">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+const TodoItem = ({ todo }) => {
+  return (
+    <div className="flex items-center justify-between h-8 text-sm">
+      <div className="flex items-center gap-2">
+        <input type="checkbox" readOnly checked={todo.isDone} />
+        <div className="text-lg">{todo.content}</div>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="text-gray-600">{todo.date}</div>
+        <button className="border rounded-3xl px-4 py-1 h-full bg-neutral-300 text-white text-xs">
+          삭제
+        </button>
+      </div>
+    </div>
+  );
+};
+TodoItem.propTypes = {
+  todo: PropTypes.array.isRequired,
+};
+
+export default TodoItem;
