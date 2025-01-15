68bcfd37625a033915986b3ca4d61896d58aeceb
bye0nzn <byeonayun@ewhain.net>
2025-01-15 15:22:57 +0900

    feat: 날씨 API 연결&아이콘 가져오기기

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,23 +1,30 @@
+
 import PropTypes from "prop-types";
 
-const TodoItem = ({ todo, onUpdate }) => {
-  const onChangeCheckbox = () =>{
-      onUpdate(todo.id);
+const TodoItem = ({ todo, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(todo.id);
+  };
+  const onClickDelete = () => {
+    onDelete(todo.id);
   };
 
   return (
     <div className="flex items-center justify-between h-8 text-sm">
       <div className="flex items-center gap-2">
-        <input 
-          type="checkbox" 
+        <input
+          type="checkbox"
           onChange={onChangeCheckbox}
           checked={todo.isDone}
-      />
+        />
         <div className="text-lg">{todo.content}</div>
       </div>
       <div className="flex items-center gap-2">
         <div className="text-gray-600">{todo.date}</div>
-        <button className="border rounded-3xl px-4 py-1 h-full bg-neutral-300 text-white text-xs">
+        <button
+          onClick={onClickDelete}
+          className="border rounded-3xl px-4 py-1 h-full bg-neutral-300 text-white text-xs"
+        >
           삭제
         </button>
       </div>
@@ -26,6 +33,8 @@ const TodoItem = ({ todo, onUpdate }) => {
 };
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
