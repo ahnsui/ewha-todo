4c2665f015255675808990e3e2407f506f663a5a
amyleeee <dlekdls5577@gmail.com>
2025-01-17 19:11:33 +0900

    [이다인] Feat: 날씨 API 연결 및 웹스토리지 사용 (#28)
    * feat 리액트 프로젝트 생성
    
    * feat: 전체적인 UI 구현
    
    * [이다인] Feat: todo 리스트 추가 기능 구현
    
    * feat: todo 리스트 수정 기능 구현 (onUpdate까지)
    
    * feat: 날씨 API 연결 및 웹스토리지 사용
    
    ---------
    
    Co-authored-by: [dainlee] <[dlekdls5577@gmail.com]>
    Co-authored-by: ahnsui <139625508+ahnsui@users.noreply.github.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,39 @@
+import { useState } from "react";
 import { getDate } from "../utils/getDate";
+import axios from "axios";
 
 const Header = () => {
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}`;
   const today = new Date();
+  const [weather, setWeather] = useState(""); //weather는 이미지 url
+
+  axios
+    .get(URL)
+    .then((response) => {
+      const weatherIcon = response.data.weather[0].icon; //50d
+      const weatherIConAdrs = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      setWeather(weatherIConAdrs);
+      console.log(weather);
+    })
+    .catch((err) => {
+      console.log("발생한 오류: ", err);
+    });
+
   return (
-    <div className = "flex justify-between mt-14">
-      <div className = "flex flex-col text-2xl font-semibold">
-        <h1> {getDate(today)} </h1>
-        <div className = "flex">
+    <div className="flex justify-between mt-14">
+      <div className="flex flex-col text-2xl font-semibold">
+        <h1>{getDate(today)}</h1>
+        <div className="flex items-center">
           <h1>오늘의 날씨</h1>
-          <h1>아이콘</h1>
+          <h1>
+            <img alt="weatherIcon" src={weather} className="w-14 h-14" />
+          </h1>
         </div>
       </div>
-      <img className="w-16" alt="rocket" src="/Emoji.png"/>
+      <img className="w-16" alt="rocket" src="/Emoji.png" />
     </div>
   );
-
 };
 
 export default Header;
